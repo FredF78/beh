1a60f28e99c35566eb9784dc2a150f3cd02206e1
Fredrik Forsberg <fredrik.forsberg@skolverket.se>
2020-01-31 15:15:30 +0100

    more tests, empty objs added to tete inriktning

--- spec/validjson.spec.js
+++ spec/validjson.spec.js
@@ -1,12 +1,17 @@
+//import { yrken, utbildningar as _utbildningar } from '../behorighetsvisaren-data-20200131-1019.json';
 const file = require('../behorighetsvisaren-data-20200131-1019.json');
+const assert = require('assert');
+const yrken = file.yrken;
+const _utbildningar = file.utbildningar;
+
 describe('behorighetsvisaren', () => {
     it("should contain 70 yrken", () => {
-        expect(file.yrken.length).toBe(70);
+        expect(yrken.length).toBe(70);
     });
    
     it("should contain correct utbildningar", () => {
         let utbildningar = [];
-        file.utbildningar.map(utb => {
+        _utbildningar.map(utb => {
             utbildningar.push(utb.programkod);
         });
         expect(utbildningar.length).toBe(6);
@@ -14,7 +19,7 @@ describe('behorighetsvisaren', () => {
 
     it("should contain correct inriktningar", () => {
         let inriktningar = [];
-        file.utbildningar.map(utb => {
+        _utbildningar.map(utb => {
             utb.inriktningar.map(inr => {
                 inriktningar.push(inr.inriktningskod)
             })
@@ -22,6 +27,37 @@ describe('behorighetsvisaren', () => {
         expect(inriktningar.length).toBe(11);
     });
 
+    it('should contain kursarray in every yrke', () => {
+        function parseYrkesKurser() {
+            yrken.map(yrke => {
+                Array.isArray(yrke.kurser);
+            });
+        }
+        expect(function(){parseYrkesKurser();}).not.toThrowError();
+    });
+
      // fyll på med tester
+     it('should contain inriktningar and behorigheter', () => {
+         function assertOInriktningarLength() {
+             _utbildningar.map(utb => {
+                assert(utb.inriktningar.length > 0);
+             });
+         }
+
+         function assertBehorigheter() {
+             _utbildningar.map(utb => {
+                console.log(utb.programkod);
+                utb.inriktningar.map(inr => {
+                    console.log(inr.inriktningskod);
+                    assert(inr.behorigheter[0].hasOwnProperty('A'));
+                    assert(inr.behorigheter[1].hasOwnProperty('B'));
+                    assert(inr.behorigheter[2].hasOwnProperty('C'));
+                });
+            });
+         }
+
+         expect(function() {assertOInriktningarLength();}).not.toThrowError();
+         expect(function() {assertBehorigheter();}).not.toThrowError();
+     })
 
 });
