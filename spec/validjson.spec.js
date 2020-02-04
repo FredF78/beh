73cce9fc44648e49285e51e2005aba5b82066e32
Fredrik Forsberg <fredrik.forsberg@skolverket.se>
2020-02-04 11:10:00 +0100

    new data files, tests fixed

--- spec/validjson.spec.js
+++ spec/validjson.spec.js
@@ -1,8 +1,7 @@
 //import { yrken, utbildningar as _utbildningar } from '../behorighetsvisaren-data-20200131-1019.json';
-const file = require('../behorighetsvisaren-data-20200131-1019.json');
+const yrken = require('../datastore-competenceguide-titles-20200204-1000.json');
+const _utbildningar = require('../datastore-competenceguide-programs-20200204-1000.json');
 const assert = require('assert');
-const yrken = file.yrken;
-const _utbildningar = file.utbildningar;
 
 describe('behorighetsvisaren', () => {
     it("should contain 70 yrken", () => {
@@ -48,10 +47,10 @@ describe('behorighetsvisaren', () => {
              _utbildningar.map(utb => {
                 console.log(utb.programkod);
                 utb.inriktningar.map(inr => {
-                    console.log(inr.inriktningskod);
-                    assert(inr.behorigheter[0].hasOwnProperty('A'));
-                    assert(inr.behorigheter[1].hasOwnProperty('B'));
-                    assert(inr.behorigheter[2].hasOwnProperty('C'));
+                    //console.log(inr.behorigheter[1]['kategori']);
+                    assert(inr.behorigheter[0]['kategori'] ==='A');
+                    assert(inr.behorigheter[1]['kategori'] === 'B');
+                    assert(inr.behorigheter[2]['kategori'] === 'C');
                 });
             });
          }
