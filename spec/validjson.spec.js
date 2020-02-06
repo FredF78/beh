0f8b61328fc9df5c597eca411381c7e6ed0b08b6
Fredrik Forsberg <fredrik.forsberg@skolverket.se>
2020-02-06 13:50:31 +0100

    tests updated

--- spec/validjson.spec.js
+++ spec/validjson.spec.js
@@ -1,6 +1,6 @@
 //import { yrken, utbildningar as _utbildningar } from '../behorighetsvisaren-data-20200131-1019.json';
 const yrken = require('../datastore-competenceguide-titles-20200204-1000.json');
-const _utbildningar = require('../datastore-competenceguide-programs-20200204-1000.json');
+const _utbildningar = require('../datastore-competenceguide-programs-20200206-1320.json');
 const assert = require('assert');
 
 describe('behorighetsvisaren', () => {
@@ -47,10 +47,22 @@ describe('behorighetsvisaren', () => {
              _utbildningar.map(utb => {
                 console.log(utb.programkod);
                 utb.inriktningar.map(inr => {
+                    const actualYrken = [];
                     //console.log(inr.behorigheter[1]['kategori']);
                     assert(inr.behorigheter[0]['kategori'] ==='A');
                     assert(inr.behorigheter[1]['kategori'] === 'B');
                     assert(inr.behorigheter[2]['kategori'] === 'C');
+                    inr.behorigheter[0]['yrken'].map(yrke => {
+                        actualYrken.push(yrke.yrkeskod);
+                    });
+                    inr.behorigheter[1]['yrken'].map(yrke => {
+                        actualYrken.push(yrke.yrkeskod);
+                    });
+                    inr.behorigheter[2]['yrken'].map(yrke => {
+                        actualYrken.push(yrke.yrkeskod);
+                    });
+                    
+                    assert(yrken.length === 66);
                 });
             });
          }
