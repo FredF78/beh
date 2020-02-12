196ead19ebe0d0c5b65e10c4e4db51f17e4969e8
Fredrik Forsberg <fredrik.forsberg@skolverket.se>
2020-02-12 14:36:56 +0100

    fixes

--- spec/validjson.spec.js
+++ spec/validjson.spec.js
@@ -1,6 +1,6 @@
 //import { yrken, utbildningar as _utbildningar } from '../behorighetsvisaren-data-20200131-1019.json';
-const yrken = require('../datastore-competenceguide-titles-20200204-1000.json');
-const _utbildningar = require('../datastore-competenceguide-programs-20200206-1320.json');
+const yrken = require('../datastore-competenceguide-titles-20200211-1502.json');
+const _utbildningar = require('../datastore-competenceguide-programs-20200211-1502.json');
 const assert = require('assert');
 
 describe('behorighetsvisaren', () => {
